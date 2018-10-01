0e11e61e3578e7cb5823d29aed622a9fe52bd15a
Adam Parkin <adam.parkin@bambora.com>
2018-10-01 08:56:49 -0700

    Revise sample buildgraph script

--- buildgraph.sh
+++ buildgraph.sh
@@ -2,8 +2,17 @@
 
 set -e
 
+if [ $# -eq 0 ]
+  then
+    echo "Must supply build number"
+    exit 1
+fi
+
 echo "Making output dir"
 mkdir -p output
+
+echo "Clearing output dir"
+rm output/*
 
 echo "Copying pylint-report.txt to output..."
 cp pylint-report.txt output/
@@ -14,5 +23,8 @@ docker build -t matplotlib:latest .
 echo "Generating graph"
 docker run --rm -v $PWD/output:/tmp matplotlib:latest python3 generate_graph.py
 
+echo "renaming graph"
+mv output/pylint.png pylint$1.png
+
 echo "Opening graph"
-open output/pylint.png
+open output/pylint$1.png
